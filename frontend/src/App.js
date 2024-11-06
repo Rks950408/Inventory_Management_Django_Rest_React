20f866cd0cf673d7689780617facd9fe76900435
Rahul Kumar <rks950408@gmail.com>
2024-11-05 17:04:21 -1100

    tailwind fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './App.css'; // Your global styles
+import './index.css'; // Tailwind CSS
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <div className="App bg-indigo-500 text-white p-4">
+<h1 className="text-3xl font-bold text-red-500">Hello, Tailwind CSS!</h1>
+</div>
   );
 }
 
