e00dc23bc0efd911187adf8cd7972b4ae6e3f3e6
Rahul Kumar <rks950408@gmail.com>
2024-11-11 18:15:52 -1100

    make accounts

---
+++ backend/Inventory_management/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from .views import register, login, logout, forgot_password, user_detail, change_password
+
+urlpatterns = [
+    # path('register/', register, name='register'),
+    # path('login/', login, name='login'),
+    # path('logout/', logout, name='logout'),
+    # path('forgot-password/', forgot_password, name='forgot_password'),
+    # path('user/', user_detail, name='user_detail'),
+    # path('change-password/', change_password, name='change_password'),
+]
